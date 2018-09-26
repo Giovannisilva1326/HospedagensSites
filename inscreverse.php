4210891a133e5c867252b55b0f31b31918ae7340
Giovanni <you@example.com>
2018-09-26 20:47:53 -0300

    global

--- inscreverse.php
+++ inscreverse.php
@@ -177,7 +177,7 @@ include 'css.php';
             <label>Email:</label>
           </div>
           <div class="col-3">
-            <input type="email" name="emailCad" id="emailCad" class="form-control" placeholder="Email">
+            <input type="email" name="emailCad" id="emailCad" class="form-control" onblur="validacaoEmail(emailCad.email)" placeholder="teste">
 
             <?
             if($erro_email){
@@ -236,6 +236,28 @@ include 'css.php';
     return false;
   }
   document.querySelector('#formCadUser').submit()
+  }
+
+  function validacaoEmail(field) {
+  usuario = field.value.substring(0, field.value.indexOf("@"));
+  dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
+ 
+  if ((usuario.length >=1) &&
+    (dominio.length >=3) && 
+    (usuario.search("@")==-1) && 
+    (dominio.search("@")==-1) &&
+    (usuario.search(" ")==-1) && 
+    (dominio.search(" ")==-1) &&
+    (dominio.search(".")!=-1) &&      
+    (dominio.indexOf(".") >=1)&& 
+    (dominio.lastIndexOf(".") < dominio.length - 1)) {
+  document.getElementById("msgemail").innerHTML="E-mail válido";
+  alert("E-mail valido");
+  }
+  else{
+  document.getElementById("msgemail").innerHTML="<font color='red'>E-mail inválido </font>";
+  alert("E-mail invalido");
+  }
   }
 </script>
 </body>
